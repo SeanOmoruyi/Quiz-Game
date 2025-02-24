317a15ec14f09b63fa2b9bd7b9b98a4263422597
SeanOmoruyi <C22346841@mytudublin.ie>
2025-02-24 12:21:50 +0000

    Update QuizGame.py

--- QuizGame.py
+++ QuizGame.py
@@ -1,4 +1,6 @@
 def quiz_game():
     questions = [
-        {}
+        {"questions": "What is the capital of France", "options": ["A. Paris", "B. Rome", "C. Madrid"], "answer": "A"},
+        {"questions": "What is 5 + 7?", "options": ["A. 10", "B. 12", "C, 14"], "answer": "B"},
+        
     ]

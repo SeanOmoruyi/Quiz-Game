72c3bd1e2232e281cfeb6644adf8854514105710
SeanOmoruyi <C22346841@mytudublin.ie>
2025-02-24 12:29:38 +0000

    Update QuizGame.py

--- QuizGame.py
+++ QuizGame.py
@@ -2,5 +2,6 @@ def quiz_game():
     questions = [
         {"questions": "What is the capital of France", "options": ["A. Paris", "B. Rome", "C. Madrid"], "answer": "A"},
         {"questions": "What is 5 + 7?", "options": ["A. 10", "B. 12", "C, 14"], "answer": "B"},
-        
+        {"questions": "Which planet is known as the Red Planet?", "options": ["A. Venus", "B. Mars", "C. Mercury"], "answer": "B"},
+        {"questions": "What year was the French Revolution?", "options": ["A. 1892", "B. 1704", "C. 1789"], "answer": "C"}
     ]
